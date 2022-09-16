68fdb16976abb6dbb9a79476000ec7d855227eaf
vsevaq <vsevolodq@gmail.com>
2022-09-16 22:47:02 +0300

    Added nice description to ReadMe

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-# listview_and_builder
+<h1 align="center">Flutter Layouts Challenge APP</h1>
 
-A new Flutter project.
+<p><img alt="An iphone Screen Record" src="ScreenRec.gif" height="550" /></p>
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,9 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final List _posts = ['Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5'];
+  final List _posts = ['Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5', 'Post 6'];
 
-  final List _stories = ['Story 1', 'Story 2', 'Story 3', 'Story 4', 'Story 5'];
+  final List _stories = ['Story 1', 'Story 2', 'Story 3', 'Story 4', 'Story 5', 'Story 6', 'Story 7', 'Story 8'];
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              
+
               // instagram stories
               Container(
                 height: 120,
